52bee3b31e6d5c396bdbff77b194dbbeaa163c0b
brianbrix <mokandubrian@gmail.com>
2025-05-19 08:17:14 +0300

    Remove multi-module to simple monolith

--- src/main/java/com/afyaquik/AfyaquikApplication.java
+++ src/main/java/com/afyaquik/AfyaquikApplication.java
@@ -9,10 +9,7 @@ import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
-//@EntityScan(basePackages = {"com.afyaquik.users.entity", "com.afyaquik.patients.entity"})
-//@EnableJpaRepositories(basePackages = {"com.afyaquik.users.repository","com.afyaquik.patients.repository"})
 @EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
-@ComponentScan(basePackages = {"com.afyaquik.users","com.afyaquik.patients", "com.afyaquik.web", "com.afyaquik.utils","com.afyaquik.appointments"})
 @EnableCaching
 public class AfyaquikApplication {
 
